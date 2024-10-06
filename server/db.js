2ee6aec410827bcd4fcf7a81091e52fcf0d36ab2
Chanson Tang <90084138+chanchantang@users.noreply.github.com>
2024-10-06 02:31:12 -0700

    Reverted change to db file

--- server/db.js
+++ server/db.js
@@ -1,17 +1,17 @@
 const Pool = require("pg").Pool;
 
-// var parse = require("pg-connection-string").parse;
+var parse = require("pg-connection-string").parse;
 
-// var config = parse(process.env.DATABASE_URL);
+var config = parse(process.env.DATABASE_URL);
 
-// const pool = new Pool(config);
+const pool = new Pool(config);
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "postgres",
-  port: 5433,
-});
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "postgres",
+//   password: "postgres",
+//   port: 5433,
+// });
 
 module.exports = pool;
