f3bfd2c1ce99b37c746d707a3ae371aea1c3d2fa
Francis Ferrer <francismjferrer@gmail.com>
2024-10-06 02:55:22 -0700

    modified table

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -5,6 +5,14 @@ import Edit from "./Edit";
 const List = () => {
   const [todos, setTodos] = useState([]);
 
+  const CheckboxExample = () => {
+    const [isChecked, setIsChecked] = useState(false);
+  
+    const handleCheckboxChange = () => {
+      setIsChecked(!isChecked);
+    };
+  };
+
   //delete todo function
 
   const deleteTodo = async id => {
@@ -42,7 +50,10 @@ const List = () => {
       <table className="table mt-5 text-center">
         <thead>
           <tr>
+            <th>Completed</th>
             <th>Description</th>
+            <th>Experience</th>
+            <th>Type</th>
             <th>Edit</th>
             <th>Delete</th>
           </tr>
@@ -50,7 +61,14 @@ const List = () => {
         <tbody>
           {todos.map(todo => (
             <tr key={todo.todo_id}>
+              <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleCheckboxChange}
+              />
               <td>{todo.description}</td>
+              <td>{todo.experience}</td>
+              <td>{todo.type}</td>
               <td>
                 <Edit todo={todo} />
               </td>
